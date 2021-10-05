1d45e06b518161468e7fbc4b480ec4fb75c17bd4
Fabian Kirschner <fabian.kirschner@gmail.com>
2021-10-05 20:55:12 +0200

    added static website

--- pulumi/quickstart_azure_ts/index.ts
+++ pulumi/quickstart_azure_ts/index.ts
@@ -3,18 +3,35 @@ import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 
 // Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+const rg = new resources.ResourceGroup("pulumiResourceGroup");
 
 // Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
+const sa = new storage.StorageAccount("krschepulumisa", {
+    resourceGroupName: rg.name,
     sku: {
         name: storage.SkuName.Standard_LRS,
     },
-    kind: storage.Kind.StorageV2,
+    kind: storage.Kind.StorageV2
 });
 
 // Export the primary key of the Storage Account
-const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
+const storageAccountKeys = pulumi.all([rg.name, sa.name]).apply(([resourceGroupName, accountName]) =>
+storage.listStorageAccountKeys({ resourceGroupName, accountName }));
 export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
+// Enable Website and supply index.html
+const saWebsite = new storage.StorageAccountStaticWebsite("sawebsite", {
+    accountName: sa.name,
+    resourceGroupName: rg.name,
+    indexDocument: "index.html"
+})
+
+const blobWebsite = new storage.Blob("index.html", {
+    resourceGroupName: rg.name,
+    accountName: sa.name,
+    containerName: saWebsite.containerName,
+    source: new pulumi.asset.FileAsset("index.html"),
+    contentType: "text/html"
+})
+
+export const websiteEndpoint = sa.primaryEndpoints.web;
