a492a649825dd0218146c0c02f9c695e137b88e6
Fabian Kirschner <fabian.kirschner@gmail.com>
2022-02-11 01:18:02 +0100

    Added a csharp minimum console app
    with new top-level statements :)

---
+++ csharp/README.md
@@ -0,0 +1,10 @@
+# C#
+
+## dotnet cli
+```bash
+# Create new (console) project in current dir
+dotnet new console
+
+# Build prj in current dir
+dotnet build 
+```
---
+++ csharp/console-bare-minimum/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is a single line C# Console App! Now possible with 'top-level statements'");
