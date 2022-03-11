8c6abe687c1a16206a12c23308d2ff3be112f43c
Fabian Kirschner <fabian.kirschner@gmail.com>
2022-03-11 01:09:15 +0100

    added c# signalr-getting-started

---
+++ csharp/signalr-getting-started/README.md
@@ -0,0 +1,3 @@
+# SignalR Getting Started
+
+followed gsg from here: https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-6.0&tabs=visual-studio
---
+++ csharp/signalr-getting-started/signalr-getting-started/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace signalr_getting_started.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
