f621f593001e383426fea6caf2341768e414b6c7
Fabian Kirschner <fabian.kirschner@gmail.com>
2022-06-19 03:47:29 +0200

    feat: add react-tailwind tryout

---
+++ web/react-tailwind/README.md
@@ -0,0 +1,5 @@
+# React Tailwind
+
+- Create with `create-react-app`
+- Followed this [vid](https://www.youtube.com/watch?v=pfaSUYaSgRo)
+- `craco` overwrites default `postcss` config
---
+++ web/react-tailwind/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" bg-slate-200 dark:bg-slate-800 container mx-auto">
+      <h1 className="text-3xl font-bold underline text-center">Moin</h1>
+    </div>
+  );
+}
+
+export default App;
